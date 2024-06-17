26dff8a24df29bb0ea0fe84cfca5597fc84a9f39
agwadan <agwa.dan@gmail.com>
2024-06-17 15:17:23 +0300

    Update heading size for small screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import { Container, Typography, Box, styled } from "@mui/material";
 import BookSearchBar from "./components/BookSearchBar";
 
-const AppContainer = styled(Container)({
+const AppContainer = styled(Container)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
-});
+}));
+
+const ResponsiveHeading = styled(Typography)(({ theme }) => ({
+  color: "#335C6E",
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "1.5rem",
+    margin: "1.5rem 0",
+    textAlign: "center",
+  },
+}));
 
 const App: React.FC = () => {
   return (
@@ -16,9 +25,9 @@ const App: React.FC = () => {
         sx={{ maxHeight: "5rem" }}
         src="https://github.com/ElloTechnology/backend_takehome/assets/3518127/561bc8d4-bffc-4360-b9ea-61e876bcec93"
       />
-      <Typography variant="h4" gutterBottom style={{ color: "#335C6E" }}>
+      <ResponsiveHeading variant="h4" gutterBottom style={{ color: "#335C6E" }}>
         Assign A Book
-      </Typography>
+      </ResponsiveHeading>
       <BookSearchBar />
     </AppContainer>
   );
